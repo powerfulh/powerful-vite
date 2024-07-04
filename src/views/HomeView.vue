9bd96470745a8b64a30594437ad1ceb0a01e0001
powerfulh <68536042+powerfulh@users.noreply.github.com>
2024-07-04 17:01:01 +0900

    연습 끝

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,11 @@
 <script setup>
-import { reactive } from 'vue';
-import PS from 'powerful-select';
-import VDP from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css';
+//import '@vuepic/vue-datepicker/dist/main.css';
 
-const list = reactive([
-	{
-		code: 1,
-		text: 'a'
-	}
-])
 </script>
 
 <template>
 	<main>
 		<h1>Home</h1>
-		<p-s :list="list"></p-s>
-		<v-d-p></v-d-p>
+		
 	</main>
 </template>
