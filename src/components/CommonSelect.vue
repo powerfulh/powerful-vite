e149286ecb7d05dbcde1ef39f86237df81db590d
powerfulh <68536042+powerfulh@users.noreply.github.com>
2024-07-06 16:30:15 +0900

    z-index 는 css로 하는 걸로 하자

---
+++ src/components/CommonSelect.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+import { PS } from "powerful-select";
+
+const prop = defineProps({
+	list: Array,
+	modelValue: {
+		type: [String, Number],
+		required: true
+	},
+	multi: Boolean
+})
+
+const ownVal = ref(prop.modelValue)
+</script>
+
+<template>
+	<p-s v-model="ownVal" :list="list" :multi="multi" ref="ps" @update:model-value="$emit('update:model-value', ownVal)"></p-s>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { reactive, ref } from 'vue';
-import { PS } from "powerful-select";
+import CS from '@/components/CommonSelect.vue';
 
 const testList = reactive([
 	{
@@ -12,15 +12,33 @@ const testList = reactive([
 		text: 'b'
 	},
 ])
+const testList1 = reactive([
+	{
+		code: 1,
+		text: 'a'
+	},
+	{
+		code: 2,
+		text: 'b'
+	},
+	{
+		code: 3,
+		text: 'c'
+	},
+])
 const selVal = ref('')
+const selVal1 = ref('')
 </script>
 
 <template>
 	<main>
 		<h1>Home</h1>
-		<p-s v-model="selVal" :list="testList"></p-s>
+		<c-s v-model="selVal" :list="testList"></c-s>
 		selVal: {{ selVal }}
 		<br>
 		<input type="text" placeholder="Other input">
+		<br>
+		<c-s v-model="selVal1" :list="testList1" multi></c-s>
+		selVal1: {{ selVal1 }}
 	</main>
 </template>
