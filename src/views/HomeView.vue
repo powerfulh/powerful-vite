d6cd87cbe30016034849a03c2e687ba0ea206714
powerfulh <68536042+powerfulh@users.noreply.github.com>
2024-07-06 15:46:58 +0900

    기본적인 적용 예

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <script setup>
-//import '@vuepic/vue-datepicker/dist/main.css';
+import { reactive, ref } from 'vue';
+import { PS } from "powerful-select";
 
+const testList = reactive([
+	{
+		code: 1,
+		text: 'a'
+	},
+	{
+		code: 2,
+		text: 'b'
+	},
+])
+const selVal = ref('')
 </script>
 
 <template>
 	<main>
 		<h1>Home</h1>
-		
+		<p-s v-model="selVal" :list="testList"></p-s>
+		selVal: {{ selVal }}
+		<br>
+		<input type="text" placeholder="Other input">
 	</main>
 </template>
