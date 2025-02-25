d99b5b7f79a070c623c3cf1a4150b82a67157b02
hanhoon <hanhoon12@naver.com>
2025-02-25 10:09:12 +0900

    Todo

--- src/components/views/menu0/AlertConfirm.vue
+++ src/components/views/menu0/AlertConfirm.vue
@@ -10,6 +10,7 @@ const modal = reactive({
 	<main>
 		<h1>공통 모달 예제</h1>
 		<button @click="modal.basic = true">일반 모달</button>
+		<button>공통 알림 todo</button>
 
 		<c-m v-if="modal.basic" title="공통 일반 모달" @close="modal.basic = false"> 다양하게 공통으로 쓸 모달 </c-m>
 	</main>
