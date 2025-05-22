50894f649de95d79d2c790a4428cc9f0ee3a6574
hanhoon <hanhoon12@naver.com>
2025-05-22 13:21:21 +0900

    Delete unused

--- src/components/views/menu0/PowerfulSelect.vue
+++ src/components/views/menu0/PowerfulSelect.vue
@@ -2,6 +2,8 @@
 import { reactive, ref } from 'vue'
 
 const v = ref('')
+const v2 = ref('')
+const v3 = ref('')
 const l = reactive([
 	{
 		code: 'opt0',
@@ -11,14 +13,31 @@ const l = reactive([
 		code: 'opt2',
 		text: 'Second',
 	},
+	{
+		code: 'opt3',
+		text: 'Third',
+	},
 ])
+const fetchedList = reactive([])
+
+// 서버에서 옵션 가져온다는 상황으로 옵션 비동기 재설정
+setTimeout(() => {
+	fetchedList.push(...l)
+}, 2000)
 </script>
 
 <template>
 	<main>
 		<h1>파워풀 셀렉트 예제</h1>
+		<!-- 내가 만들고 기억이 안 나지만 기본적으로 1번 옵션 자동 세팅인 듯 하다 -->
+		하드 코드 옵션 구성
 		<c-s v-model="v" :list="l"></c-s>
 		<br />
-		여러 예제 만들고 나면 홈뷰에서 셀렉트 걷어내기 todo 홈 뷰의 존재의의가 필요 없다면 삭제 고려
+		서버에서 받은 옵션 구성
+		<c-s v-model="v2" :list="fetchedList"></c-s>
+		<br />
+		멀티 셀렉트
+		<!-- 멀티는 자동 세팅이 아닌 듯 하다 -->
+		<c-s v-model="v3" :list="l" multi></c-s>
 	</main>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routerUtil from '@/util/router'
-import HomeView from '@/components/views/HomeView.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomeView,
-		},
-		...routerUtil.routes,
-	],
+	routes: [...routerUtil.routes],
 })
 
 router.afterEach((to, from) => {
