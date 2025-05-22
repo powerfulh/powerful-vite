0e44dde0651a3567a273c315cfd1c5e56456fb07
hanhoon <hanhoon12@naver.com>
2025-05-22 13:23:05 +0900

    Add case

--- src/components/views/menu0/PowerfulSelect.vue
+++ src/components/views/menu0/PowerfulSelect.vue
@@ -4,6 +4,7 @@ import { reactive, ref } from 'vue'
 const v = ref('')
 const v2 = ref('')
 const v3 = ref('')
+const v4 = ref('')
 const l = reactive([
 	{
 		code: 'opt0',
@@ -39,5 +40,8 @@ setTimeout(() => {
 		멀티 셀렉트
 		<!-- 멀티는 자동 세팅이 아닌 듯 하다 -->
 		<c-s v-model="v3" :list="l" multi></c-s>
+		<br />
+		키보드 입력
+		<c-s v-model="v4" :list="l" auto></c-s>
 	</main>
 </template>
