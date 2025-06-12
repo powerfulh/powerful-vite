e826b4408afb24ba2611c6209e443755984ceab3
hanhoon <hanhoon12@naver.com>
2025-06-12 13:49:51 +0900

    Filter menu

--- src/components/app/TopHeader.vue
+++ src/components/app/TopHeader.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import { RouteRecord, RouterLink } from 'vue-router'
+
+function finalRoutes(routes: Array<RouteRecord>) {
+	return routes.filter(item => item.components)
+}
 </script>
 
 <template>
 	<header>
 		<div class="wrapper">
 			<nav style="text-align: center">
-				<router-link v-for="(item, i) in $router.getRoutes()" :key="i" :to="item.path" style="margin-right: 8px">{{
+				<router-link v-for="(item, i) in finalRoutes($router.getRoutes())" :key="i" :to="item.path" style="margin-right: 8px">{{
 					item.name || item.path
 				}}</router-link>
 			</nav>
