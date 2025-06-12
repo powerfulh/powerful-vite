8378a3474c232a83fb58f2263b4e027ad75fa159
hanhoon <hanhoon12@naver.com>
2025-06-12 14:15:26 +0900

    Sample: defineModel

---
+++ src/components/views/menu0/home/ChildModel.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const propData = defineModel()
+</script>
+
+<template>
+	<input v-model="propData" />
+</template>
--- src/components/views/menu0/home/TestView.vue
+++ src/components/views/menu0/home/TestView.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import ChildModel from './ChildModel.vue'
+
+const textData = ref('')
+</script>
 
 <template>
 	<main>
 		<h1>3.4 부터 권장한다는 defineModel 써보기</h1>
+		<ChildModel v-model="textData" />
+		{{ textData }}
+		<!-- 존나 편해졌다 -->
 	</main>
 </template>
